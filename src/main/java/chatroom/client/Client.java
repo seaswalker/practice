082f0ce9c348b5098946d62c0cb9375abfc28102
seaswalker <xsdwem7@hotmail.com>
2015-11-10 21:25:06 +0800

    修复package

--- src/main/java/chatroom/client/Client.java
+++ src/main/java/chatroom/client/Client.java
@@ -1,4 +1,4 @@
-﻿package client;
+﻿package chatroom.client;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
--- src/main/java/chatroom/server/Server.java
+++ src/main/java/chatroom/server/Server.java
@@ -1,4 +1,4 @@
-﻿package server;
+﻿package chatroom.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;

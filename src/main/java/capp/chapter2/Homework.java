c39e1d2cf0ca7e61b30717926f2f8c828d76aef5
skywalker <xsdwem7@hotmail.com>
2017-03-23 20:23:07 +0800

    caspp: 2.63-2.65

--- src/main/java/capp/chapter2/Homework.java
+++ src/main/java/capp/chapter2/Homework.java
@@ -74,7 +74,18 @@ public class Homework {
     }
 
     /**
+     * 2.63.
+     */
+    @Test
+    public void p63() {
+        int bits = 4;
+        Assert.assertEquals((-10 >> bits), sra(-10, bits));
+        Assert.assertEquals((-10 >>> bits), sr1(-10, bits));
+    }
+
+    /**
      * 用逻辑右移实现算数右移.
+     * <p>参考: https://segmentfault.com/q/1010000004895277</p>
      *
      * @param i 被移动的数字
      * @param k 右移的位
@@ -82,10 +93,63 @@ public class Homework {
      */
     private int sra(int i, int k) {
         int t = (i >>> k);
-        if (i < 0) {
-            
-        }
-        return t;
+        int mask = 1 << (32 - k - 1);
+        return (t ^ mask) - mask;
+    }
+
+    /**
+     * 用算数右移实现逻辑右移.思路:
+     * 假设有负数(正数的算数右移和逻辑右移结果是相同的)的二进制表示为:
+     * <p>1100 0011 0000，那么右移4位的结果为:</p>
+     * <p>1111 1100 0011</p>
+     * 我们只要取到原(移位之前)的符号位，即1100的1最高位1，然后
+     * 将其左移1位与算数右移的结果相加即可.
+     *
+     * @see #sra(int, int)
+     */
+    private int sr1(int i, int k) {
+        int t = i >> k;
+        int mask = (1 << (32 - k - 1)) & t;
+        return t + (mask << 1);
+    }
+
+    /**
+     * 如果一个数字(原题为无符号数)里面任何偶数位为1，那么返回1，反之返回0.
+     */
+    @Test
+    public void p64() {
+        Assert.assertEquals(anyEvenOne(8), 0);
+    }
+
+    /**
+     * @param i 正数
+     * @see #p64()
+     */
+    private int anyEvenOne(int i) {
+        return (i & 0x55555555) > 0 ? 1 : 0;
+    }
+
+    /**
+     * 2.65，判断给定的无符号数中是否有偶数个1.
+     */
+    @Test
+    public void p65() {
+        Assert.assertTrue(evenOnes(12));
+        Assert.assertTrue(!evenOnes(2));
+    }
+
+    /**
+     * 如果有偶数个1，那么必定有偶数个0，那么最终各位异或的结果应该是0.
+     *
+     * @see #p65()
+     */
+    private boolean evenOnes(int i) {
+        i ^= (i >>> 16);
+        i ^= (i >>> 8);
+        i ^= (i >>> 4);
+        i ^= (i >>> 2);
+        i ^= (i >>> 1);
+        return ((i & 1) == 0);
     }
 
 }
